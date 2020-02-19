a6fdc5900a059c4c047a6402aab42744b2f55279
paulolimadev <paulolimadf@gmail.com>
2020-02-19 15:34:07 -0300

    findById implementation

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -10,19 +10,19 @@ public class Seller implements Serializable {
 	private String name;
 	private String email;
 	private Date birthDate;
-	private Double daseSalary;
+	private Double baseSalary;
 	
 	private Department department;
 	
 	public Seller() {		
 	}
 
-	public Seller(Integer id, String name, String email, Date birthDate, Double daseSalary, Department department) {
+	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
 		this.birthDate = birthDate;
-		this.daseSalary = daseSalary;
+		this.baseSalary = baseSalary;
 		this.department = department;
 	}
 
@@ -58,12 +58,12 @@ public class Seller implements Serializable {
 		this.birthDate = birthDate;
 	}
 
-	public Double getDaseSalary() {
-		return daseSalary;
+	public Double getBaseSalary() {
+		return baseSalary;
 	}
 
-	public void setDaseSalary(Double daseSalary) {
-		this.daseSalary = daseSalary;
+	public void setBaseSalary(Double baseSalary) {
+		this.baseSalary = baseSalary;
 	}
 
 	public Department getDepartment() {
@@ -101,8 +101,8 @@ public class Seller implements Serializable {
 
 	@Override
 	public String toString() {
-		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", daseSalary="
-				+ daseSalary + ", department=" + department + "]";
+		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
+				+ baseSalary + ", department=" + department + "]";
 	}
 	
 	
